020425b3633ab4ca127f185add8a9337eae984b7
Edwar Velásquez <ricoprogramar@gmail.com>
2026-05-11 11:37:49 -0500

    chore(base): codigo base para las actividades desarrolladas en las sesiones de formacion

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-green-800 flex items-center justify-center">
+      <h1 className="text-white text-4xl font-bold">
+        Con Rico Programar Tailwind V4 funciona full...
+      </h1>
+    </div>
+  );
+}
+
+
